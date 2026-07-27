12f0b00775b4285308b79e9860abb43a56dbd45d
minseo-404 <rawfish04@gmail.com>
2026-07-27 21:04:04 +0900

    chore : revise capital city data

--- NLIC/sql/capital_area_cargo_flow.sql
+++ NLIC/sql/capital_area_cargo_flow.sql
@@ -1,38 +1,54 @@
-WITH base_data AS (
+WITH departure_data AS (
+    -- 1. [Outbound] Freight volume originating from Capital Region (Includes internal moves)
     SELECT
         연도 AS year,
-        -- Set baseline region to 'Capital Region'
-        '수도권' AS base_region,    
+        '수도권' AS base_region,
         CASE 
-            WHEN 기준지역 IN ('서울', '경기', '인천') THEN 
-                CASE WHEN 대상지역 IN ('서울', '경기', '인천') THEN '수도권' ELSE 대상지역 END
-            ELSE 
-                CASE WHEN 기준지역 IN ('서울', '경기', '인천') THEN '수도권' ELSE 기준지역 END
+            WHEN 대상지역 IN ('서울', '경기', '인천') THEN '수도권'
+            ELSE 대상지역 
         END AS target_region,
-        CASE 
-            WHEN 기준지역 IN ('서울', '경기', '인천') THEN '출발'
-            ELSE '도착'
-        END AS flow_type,
+        '출발' AS flow_type,
         물동량 AS cargo_volume
     FROM
         data_logistics_total_national
     WHERE
         기준지역 IN ('서울', '경기', '인천')
-        OR 대상지역 IN ('서울', '경기', '인천')
+),
+arrival_data AS (
+    -- 2. [Inbound] Freight volume destined for Capital Region (Includes internal moves)
+    SELECT
+        연도 AS year,
+        '수도권' AS base_region,
+        CASE 
+            WHEN 기준지역 IN ('서울', '경기', '인천') THEN '수도권'
+            ELSE 기준지역 
+        END AS target_region,
+        '도착' AS flow_type,
+        물동량 AS cargo_volume
+    FROM
+        data_logistics_total_national
+    WHERE
+        대상지역 IN ('서울', '경기', '인천')
+),
+combined_data AS (
+    -- Combine outbound and inbound dataset
+    SELECT * FROM departure_data
+    UNION ALL
+    SELECT * FROM arrival_data
 )
 SELECT
     year,
-    base_region,   -- Always '수도권' (Capital Region)
+    base_region,   -- 'Capital Region'
     target_region, -- Partner region (e.g., Gangwon, Gyeongnam, Capital Region)
-    flow_type,     -- Flow direction (Departure / Arrival)
+    flow_type,     -- Flow direction ('Departure' / 'Arrival')
     SUM(cargo_volume) AS total_cargo_volume
 FROM
-    base_data
+    combined_data
 GROUP BY
     year,
-    flow_type,
     base_region,
-    target_region
+    target_region,
+    flow_type
 ORDER BY
     year ASC,
     flow_type ASC,
